873990f48b6cc4fb78581db6b6a8a25387848bcd
HeratZaveri <39574177+HeratZaveri@users.noreply.github.com>
2020-04-29 19:20:53 -0400

    Add files via upload

--- Problem 3/Main.java
+++ Problem 3/Main.java
@@ -20,15 +20,15 @@ class Graph {
       this.myAdjList = new ArrayList<>();
     }
     void addNode(final int nodeVal){
-        Node value = new Node(nodeVal);
-        myAdjList.add(value);
+        Node val = new Node(nodeVal);
+        myAdjList.add(val);
     }
     void addUndirectedEdge(final Node first, final Node second){
-        if(myAdjList.contains(first) && myAdjList.contains(second) && notTheSame(first,second) && !(first.neighbors.contains(second))){
+        if(myAdjList.contains(first) && myAdjList.contains(second) && notTheSame(first,second) 
+            && !(first.neighbors.contains(second)) && !(second.neighbors.contains(first)))
+        {
             first.neighbors.add(second);
-          if(!(second.neighbors.contains(first))){
             second.neighbors.add(first);
-          }
         }
     }
     void removeUndirectedEdge(final Node first, final Node second){
@@ -77,9 +77,11 @@ class Graph {
 }
 
 class Main {
-  /*
   public static void main(final String[] args) {
     ArrayList<Node> myList = new ArrayList<>();
+    ArrayList<Node> myListDfsIter = new ArrayList<>();
+    ArrayList<Node> myListBftIter = new ArrayList<>();
+    /*
     //GraphSearch tools = new GraphSearch();
     Graph graph = new Graph();
     graph.addNode(1);
@@ -113,28 +115,45 @@ class Main {
     //graph.addUndirectedEdge(eight, five);
     graph.printMyGraph();
     
-  
+   */
+
+    Graph myGraph = createRandomUnweightedGraph(15);
     GraphSearch tools = new GraphSearch();
-    //myList = tools.DFSRec(two, seven);
-    //myList = tools.DFSIter(two, seven);
-    //myList = tools. BFTIter(graph);
+    Node four = myGraph.getNode(4);
+    Node twelve = myGraph.getNode(12);
+
+    //System.out.println("DFS Recursive 4 to 12: ");
+    //myList = tools.DFSRec(four, twelve);
+    //printList(myList);
+    //System.out.println();
+
+    //System.out.println("DFS Iterative 4 to 12: ");
+    //myListDfsIter = tools.DFSIter(four, twelve);
+    //printList(myListDfsIter);
+    //System.out.println();
+
+    //System.out.println("BFT Iter: ");
+    //myListBftIter = tools.BFTIter(myGraph);
+    //printList(myListBftIter);
+    //System.out.println();  
+  }
+  
+  static void printList(ArrayList<Node> myList){
     for(Node value: myList){
       System.out.print(value.data + " ");
     }
     System.out.println();
   }
-  */
+
   static Graph createRandomUnweightedGraph(final int n){
-       //ArrayList<Integer> myList = new ArrayList<>();
-       //Random rand = new Random();
        final Graph graph = new Graph();
 
        for(int i = 1; i < n; i++){
              graph.addNode(i);
        }
       for(Node first: graph.myAdjList){   
-        int lower = ThreadLocalRandom.current().nextInt(1,Math.floorDiv(n, 2));
-        int range = ThreadLocalRandom.current().nextInt(Math.floorDiv(n, 2)+1,n);
+        int lower = ThreadLocalRandom.current().nextInt(1,Math.floorDiv(n,2));
+        int range = ThreadLocalRandom.current().nextInt(Math.floorDiv(n,2)+1,n);
         List<Node> randomizedNodeList = createRandomList(graph.myAdjList,lower,range);
          for(Node second: randomizedNodeList){
               graph.addUndirectedEdge(first, second);
@@ -166,10 +185,21 @@ class Main {
 class GraphSearch {
 
   public GraphSearch(){}
+  
+  //sets all nodes to unvisited once we finish our function so they can be reused
+  void setAllNodeToDefault(ArrayList<Node> traversed){
+      for(Node visit: traversed){
+        if(visit.isVisited){
+          visit.isVisited = false;
+        }
+      }
+  }
 
   ArrayList<Node> DFSRec(final Node start, final Node end){
       ArrayList<Node> search = new ArrayList<Node>();
-      return dfsRecHelp(start, end, search);
+      search = dfsRecHelp(start, end, search);
+      setAllNodeToDefault(search);
+      return search;
   }
   ArrayList<Node> dfsRecHelp(final Node begin, final Node find, final ArrayList<Node> myList){
       if(!(myList.contains(find))){
@@ -203,7 +233,8 @@ class GraphSearch {
               }
           }
       }
-      return search;
+      setAllNodeToDefault(search);
+      return search; 
   }
 /*
   ArrayList<Node> BFTRec(Graph graph){
@@ -216,16 +247,17 @@ class GraphSearch {
   }
   
 */
+
   ArrayList<Node> BFTIter(Graph graph){
       ArrayList<Node> traversal = new ArrayList<Node>();
       Deque<Node> myQueue = new ArrayDeque<>();
-      for(Node vertex: graph.getAllNodes()){
+      for(Node vertex: graph.myAdjList){
           if(!(vertex.isVisited)){
               vertex.isVisited = true;
-              myQueue.addLast(vertex);
+              myQueue.addFirst(vertex);
               //vertex.isVisited = true;
               while(!(myQueue.isEmpty())){
-                  Node curr = myQueue.removeFirst();
+                  Node curr = myQueue.removeLast();
                   traversal.add(curr);
                   for(Node nextDoor: vertex.neighbors){
                       if(!(nextDoor.isVisited)){
@@ -236,6 +268,7 @@ class GraphSearch {
               }
           }
       }
+      setAllNodeToDefault(traversal);
       return traversal;
   }
 }
